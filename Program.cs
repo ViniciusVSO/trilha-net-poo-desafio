2ed25e6a49eb82c7fc927f9cf094385e2e8d11b7
ViniciusVSO <vinicius.so992@gmail.com>
2024-05-27 15:20:21 -0300

    Codificacao dos metodos solicitados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Samartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei:"11223344", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Samartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "Modelo 2", imei:"44332211", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
